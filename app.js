03675f99c51ff07720d94e687c3892f977e163f7
IncomprehensibleGuy <gregoryrulit@gmail.com>
2021-01-16 20:45:03 +0300

    rendering lists and cards done

--- app.js
+++ app.js
@@ -2,18 +2,94 @@
 
 const $ = {} // for including custom plugins
 
-let listCounter = 3
 
 const board = document.querySelector('.board')
-let lists = board.querySelectorAll('.list')
-let cards = board.querySelectorAll('.card')
-
 const addListBlock = board.querySelector('.add-list-block')
 const showFormBtn = addListBlock.querySelector('.show-form-btn')
 const addListForm = addListBlock.querySelector('.add-list-form')
 const formInput = addListBlock.querySelector('.add-list-input')
 const addListBtn = addListBlock.querySelector('.add-list-btn')
 const cancelBtn = addListBlock.querySelector('.add-list-cancel-btn')
+
+
+const data = {
+    lists: [
+        {
+            id: 1,
+            title: 'Super list 1',
+            cards: [
+                {
+                    title: 'Card 1'
+                },
+                {
+                    title: 'Card 2'
+                },
+                {
+                    title: 'Card 3'
+                },
+            ]
+        },
+        {
+            id: 2,
+            title: 'Super list 2',
+            cards: [
+                {
+                    title: 'Card 4'
+                },
+            ]
+        },
+        {
+            id: 3,
+            title: 'Super list 3',
+            cards: [
+                {
+                    title: 'Card 5'
+                },
+                {
+                    title: 'Card 6'
+                },
+            ]
+        },
+    ],
+}
+
+
+const cardToHTML = card => `<div class="list__item card" draggable="true">${ card.title }</div>`
+
+
+const listToHTML = list => `
+    <div id="list${ list.id }" class="list" draggable="true">
+        <div class="list__header">
+            <input type="text" class="list__title" value="${ list.title }">
+            <div class="list__options">
+                <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="more">
+                    <circle cx="5"  cy="10" r="1.2"></circle>
+                    <circle cx="10" cy="10" r="1.2"></circle>
+                    <circle cx="15" cy="10" r="1.2"></circle>
+                </svg>                
+            </div>
+        </div>
+        <div class="list__body">
+            ${ list.cards.map(cardToHTML).join('') }
+        </div>
+        <div class="list__footer">
+            <button class="add-card-btn">
+                <svg width="16" height="16" fill="#888" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="plus">
+                    <rect x="9" y="1" width="2" height="17" rx="2"></rect>
+                    <rect x="1" y="9" width="17" height="2" rx="2"></rect>
+                </svg>    
+            </button>
+        </div>
+    </div>    
+`
+
+function render() {
+    const HTML = data.lists.map(listToHTML).join('')
+    const app = document.getElementById('app')
+    app.insertAdjacentHTML('afterbegin', HTML)
+}
+
+render()
 
 
 // Drag & Drop for cards
@@ -31,6 +107,7 @@ function getBeforeCard(container, y) {
     return notDraggingElements.reduce( reducer, { offset: Number.NEGATIVE_INFINITY } ).element
 }
 
+
 function onOverList(e) {
     e.preventDefault() // enable drop event
     const beforeElement = getBeforeCard(this, e.clientY)
@@ -38,11 +115,13 @@ function onOverList(e) {
     beforeElement === null ? this.childNodes[3].appendChild(dragging) : this.childNodes[3].insertBefore(dragging, beforeElement)
 }
 
+
 function cardOnDragStart(e) {
     e.stopPropagation()
     board.removeEventListener('dragover', onOverBoard);
     this.classList.add('dragging')
 }
+
 
 function cardOnDragEnd(e) {
     e.stopPropagation()
@@ -65,6 +144,7 @@ function getBeforeList(board, x) {
     return notDraggingElements.reduce( reducer, { offset: Number.NEGATIVE_INFINITY } ).element
 }
 
+
 function onOverBoard(e) {
     e.preventDefault() // enable drop event
     const beforeList = getBeforeList(this, e.clientX)
@@ -75,9 +155,7 @@ function onOverBoard(e) {
 
 // Adding listeners
 
-board.addEventListener('dragover', onOverBoard)
-
-lists.forEach(list => { 
+function addListEvents(list) {
     list.addEventListener('dragover', onOverList)
     list.addEventListener('dragstart', () => {
         lists.forEach(list => list.removeEventListener('dragover', onOverList))
@@ -87,13 +165,23 @@ lists.forEach(list => {
         list.classList.remove('dragging-list')
         lists.forEach(list => list.addEventListener('dragover', onOverList))
     })
-})
+}
 
-cards.forEach(card => { 
+
+function addCardEvents(card) {
     card.addEventListener('dragstart', cardOnDragStart)
     card.addEventListener('dragend', cardOnDragEnd)
     card.addEventListener('click', () => console.log(1))
-})
+}
+
+
+board.addEventListener('dragover', onOverBoard)
+
+let lists = board.querySelectorAll('.list')
+let cards = board.querySelectorAll('.card')
+
+lists.forEach(list => addListEvents(list))
+cards.forEach(card => addCardEvents(card))
 
 
 // create card
@@ -177,47 +265,24 @@ function hideAddListForm() {
 const closeAddListForm = e => document.activeElement !== formInput ? hideAddListForm() : ''
 
 
+function listHTMLToNode(listHTML) {
+    const div = document.createElement('div');
+    div.innerHTML = listHTML.trim();
+    return div.firstChild; 
+}
+
+
 function createList(title) {
-
-    listCounter++
-
-    const listHTML = `
-        <div class="list__header">
-            <input type="text" class="list__title" value="${ title }">
-            <div class="list__options">
-                <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="more">
-                    <circle cx= "5" cy="10" r="1.2"></circle><circle cx= "10" cy="10" r="1.2"></circle><circle cx="15" cy="10" r="1.2"></circle>
-                </svg>                
-            </div>
-        </div>
-        <div class="list__body">
-        </div>
-        <div class="list__footer">
-            <button class="add-card-btn">
-                <svg width="16" height="16" fill="#888" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="plus">
-                    <rect x="9" y="1" width="2" height="17" rx="2"></rect><rect x="1" y="9" width="17" height="2" rx="2"></rect>
-                </svg>    
-            </button>
-        </div>
-    `
-    const list = document.createElement('div')
-    list.id = listCounter
-    list.className += 'list'
-    list.setAttribute('draggable', 'true')
-    list.innerHTML = listHTML
-    list.addEventListener('dragover', onOverList)
-    list.addEventListener('dragstart', () => {
-        lists.forEach(list => list.removeEventListener('dragover', onOverList))
-        console.log('start');
-        list.classList.add('dragging-list')
-    })
-    list.addEventListener('dragend', () => { 
-        list.classList.remove('dragging-list')
-        lists.forEach(list => list.addEventListener('dragover', onOverList))
-    })
-    list.querySelector('.add-card-btn').addEventListener('click', addTextArea)
-
-    return list
+    const newList = {
+        id: data.lists.length + 1,
+        title: title,
+        cards: [],
+    }
+    data.lists.push(newList)
+    newListNode = listHTMLToNode(listToHTML(newList))    
+    addListEvents(newListNode)
+    newListNode.querySelector('.add-card-btn').addEventListener('click', addTextArea)
+    return newListNode
 }
 
 
@@ -226,7 +291,6 @@ function addListOrHideListInput() {
         const newList = createList(formInput.value)
         board.insertBefore(newList, addListBlock)
         board.scrollLeft = board.scrollWidth
-
         lists = document.querySelectorAll('.list')
         formInput.value = ''
         formInput.focus()
@@ -240,6 +304,7 @@ cancelBtn.addEventListener('click', hideAddListForm)
 addListBtn.addEventListener('click', addListOrHideListInput)
 
 
+
 // delete list
 
 // update list
@@ -251,14 +316,13 @@ addListBtn.addEventListener('click', addListOrHideListInput)
 * drag&drop list +
 * create card +
 * delete card
-* update card
-* create list
-* delete list
-* update list
+* update card: title
 * mark card as done
-* title
 * add/remove check-list 
 * add/remove date 
 * add/remove comment
+* create list +
+* delete list 
+* update list
 * render lists and cards
 **/
