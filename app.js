2a1c78b865879aaec5472d29d34cba1fc23605d7
IncomprehensibleGuy <gregoryrulit@gmail.com>
2021-01-18 23:20:07 +0300

    AJAX get data done

--- app.js
+++ app.js
@@ -8,97 +8,77 @@ const formInput = addListBlock.querySelector('.add-list-input')
 const addListBtn = addListBlock.querySelector('.add-list-btn')
 const cancelBtn = addListBlock.querySelector('.add-list-cancel-btn')
 
+let lists,
+    cards,
+    addCardBtns
 
-const requestURL = ''
 
-
-const data = {
-    lists: [
-        {
-            id: 1,
-            title: 'Какой-то список 1',
-            cards: [
-                {
-                    title: 'Карточка 1',
-                    description: 'Описание 1',
-                    marks: ['green'],
-                    checklists: [
-                        {
-                            title: 'Чек-лист 1',
-                            items: [
-                                { done: true,  text: 'todo 1' },
-                                { done: false, text: 'todo 2' },
-                                { done: false, text: 'todo 3' },
-                            ]
-                        }
-                    ],
-                    expiration: 'Sun Jan 17 2021 23:11:31',
-                },
-                {
-                    title: 'Карточка 2',
-                    description: 'Описание 2',
-                },
-                {
-                    title: 'Карточка 3',
-                    description: 'Описание 3',
-                },
-            ]
-        },
-        {
-            id: 2,
-            title: 'Какой-то список 2',
-            cards: [
-                {
-                    title: 'Карточка 4',
-                    description: 'Описание 4',
-                },
-            ]
-        },
-        {
-            id: 3,
-            title: 'Какой-то список 3',
-            cards: [
-                {
-                    title: 'Карточка 5',
-                    description: 'Описание 5',
-                },
-                {
-                    title: 'Карточка 6',
-                    description: 'Описание 6',
-                },
-            ]
-        },
-    ],
+function sendRequest(method, url, body = null) {
+    return fetch(url).then(response => {
+        return response.json()
+    })
 }
+
+const requestURL = 'http://localhost:3000/db'
+
+let data
+
+
+sendRequest('GET', requestURL)
+    .then(d => {
+        data = d
+
+        render()
+
+        lists = board.querySelectorAll('.list')
+        cards = board.querySelectorAll('.card')
+        addCardBtns = document.querySelectorAll('.add-card-btn')
+
+        board.addEventListener('dragover', onOverBoard)
+        lists.forEach(list => addListEvents(list))
+        cards.forEach(card => addCardEvents(card))
+        addCardBtns.forEach(btn => btn.addEventListener('click', addTextArea))
+        addListBlock.addEventListener('click', () => formInput.focus())
+        showFormBtn.addEventListener('click', showAddListForm)
+        cancelBtn.addEventListener('click', hideAddListForm)
+        addListBtn.addEventListener('click', addListOrHideListInput)
+    })
+    .catch(err => console.log(err))
+
+// const body = {}
+// sendRequest('POST', requestURL, body)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+//
 
 
 const cardToHTML = card => `<div class="list__item card" draggable="true">${ card.title }</div>`
 
 
 const listToHTML = list => `
-    <div id="list${ list.id }" class="list" draggable="true">
-        <div class="list__header">
-            <input type="text" class="list__title" value="${ list.title }">
-            <div class="list__options">
-                <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="more">
-                    <circle cx="5"  cy="10" r="1.2"></circle>
-                    <circle cx="10" cy="10" r="1.2"></circle>
-                    <circle cx="15" cy="10" r="1.2"></circle>
-                </svg>                
-            </div>
+<div id="list${ list.id }" class="list" draggable="true">
+    <div class="list__header">
+        <input type="text" class="list__title" value="${ list.title }">
+        <div class="list__options">
+            <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="more">
+                <circle cx="5"  cy="10" r="1.2"></circle>
+                <circle cx="10" cy="10" r="1.2"></circle>
+                <circle cx="15" cy="10" r="1.2"></circle>
+            </svg>                
         </div>
-        <div class="list__body">
-            ${ list.cards.map(cardToHTML).join('') }
-        </div>
-        <div class="list__footer">
-            <button class="add-card-btn">
-                <svg width="16" height="16" fill="#888" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="plus">
-                    <rect x="9" y="1" width="2" height="17" rx="2"></rect>
-                    <rect x="1" y="9" width="17" height="2" rx="2"></rect>
-                </svg>    
-            </button>
-        </div>
-    </div>    
+    </div>
+    <div class="list__body">
+        ${ list.cards.map(cardToHTML).join('') }
+    </div>
+    <div class="list__footer">
+        <button class="add-card-btn">
+            <svg width="16" height="16" fill="#888" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="plus">
+                <rect x="9" y="1" width="2" height="17" rx="2"></rect>
+                <rect x="1" y="9" width="17" height="2" rx="2"></rect>
+            </svg>    
+        </button>
+    </div>
+</div>    
 `
 
 function render() {
@@ -106,8 +86,6 @@ function render() {
     const app = document.getElementById('app')
     app.insertAdjacentHTML('afterbegin', HTML)
 }
-
-render()
 
 
 // Card modal
@@ -194,7 +172,7 @@ function addListEvents(list) {
         lists.forEach(list => list.removeEventListener('dragover', onOverList))
         list.classList.add('dragging-list')
     })
-    list.addEventListener('dragend', () => { 
+    list.addEventListener('dragend', () => {
         list.classList.remove('dragging-list')
         lists.forEach(list => list.addEventListener('dragover', onOverList))
     })
@@ -205,15 +183,6 @@ function addCardEvents(card) {
     card.addEventListener('dragstart', cardOnDragStart)
     card.addEventListener('dragend', cardOnDragEnd)
 }
-
-
-board.addEventListener('dragover', onOverBoard)
-
-let lists = board.querySelectorAll('.list')
-let cards = board.querySelectorAll('.card')
-
-lists.forEach(list => addListEvents(list))
-cards.forEach(card => addCardEvents(card))
 
 
 // create card
@@ -232,8 +201,8 @@ function createCard(title) {
     newCard.className += 'list__item card'
     newCard.setAttribute('draggable', 'true')
     newCard.innerText = title
-    newCard.addEventListener('dragstart', cardOnDragStart) 
-    newCard.addEventListener('dragend', cardOnDragEnd) 
+    newCard.addEventListener('dragstart', cardOnDragStart)
+    newCard.addEventListener('dragend', cardOnDragEnd)
 
     return newCard
 }
@@ -241,7 +210,7 @@ function createCard(title) {
 
 function addCardOrHideTextArea() {
     const listBody = this.parentNode
-        
+
     if (isValidTitle(this.value)) {
         const newCard = createCard(this.value)
         listBody.insertBefore(newCard, this)
@@ -254,21 +223,17 @@ function addCardOrHideTextArea() {
 
 
 function addTextArea() {
-    const listBody = this.parentElement.parentElement.querySelector('.list__body')    
+    const listBody = this.parentElement.parentElement.querySelector('.list__body')
     const textArea = document.createElement('textarea')
     textArea.classList.add('add-card-text')
     textArea.placeholder = 'Введите заголовок для этой карточки'
     textArea.rows = 3
     textArea.addEventListener('keyup', e => (e.keyCode === 13) ? textArea.blur() : {})
-    textArea.addEventListener('blur', addCardOrHideTextArea)   
+    textArea.addEventListener('blur', addCardOrHideTextArea)
     listBody.appendChild(textArea)
     textArea.focus()
     listBody.scrollTop = listBody.scrollHeight
 }
-
-
-const addCardBtns = document.querySelectorAll('.add-card-btn')
-addCardBtns.forEach(btn => btn.addEventListener('click', addTextArea))
 
 
 // delete card
@@ -289,7 +254,7 @@ function showAddListForm() {
 
 function hideAddListForm() {
     showFormBtn.style.display = 'block'
-    addListForm.style.display = 'none'    
+    addListForm.style.display = 'none'
     document.removeEventListener('click', closeAddListForm)
 }
 
@@ -300,7 +265,7 @@ const closeAddListForm = e => document.activeElement !== formInput ? hideAddList
 function listHTMLToNode(listHTML) {
     const div = document.createElement('div');
     div.innerHTML = listHTML.trim();
-    return div.firstChild; 
+    return div.firstChild;
 }
 
 
@@ -311,7 +276,7 @@ function createList(title) {
         cards: [],
     }
     data.lists.push(newList)
-    newListNode = listHTMLToNode(listToHTML(newList))    
+    newListNode = listHTMLToNode(listToHTML(newList))
     addListEvents(newListNode)
     newListNode.querySelector('.add-card-btn').addEventListener('click', addTextArea)
     return newListNode
@@ -330,13 +295,6 @@ function addListOrHideListInput() {
 }
 
 
-addListBlock.addEventListener('click', () => formInput.focus())
-showFormBtn.addEventListener('click', showAddListForm)
-cancelBtn.addEventListener('click', hideAddListForm)
-addListBtn.addEventListener('click', addListOrHideListInput)
-
-
-
 // delete list
 
 // update list
@@ -352,7 +310,7 @@ addListBtn.addEventListener('click', addListOrHideListInput)
 * on click card show modal with card details +
 * render checklists +
 * update card title (18.01)
-* 1. AJAX GET request for data with JSONplacehoder (assign to 'data' obj)
+* 1. AJAX GET request for data and it assign to 'data' obj (json-server) +
 * 2. on blur title AJAX POST request to change title
 * description setter
 * mark card as done
